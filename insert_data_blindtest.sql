2d282adb878acc6b66031f8860906b7cd604235e
ceraud <clement.eraud@capgemini.com>
2019-09-13 17:34:54 +0200

    init bdd

---
+++ insert_data_blindtest.sql
@@ -0,0 +1,3 @@
+INSERT INTO `blindtest_devfest`.`level` VALUES (1, 'DIFFICLE');
+INSERT INTO `blindtest_devfest`.`level` VALUES (2, 'MOYEN');
+INSERT INTO `blindtest_devfest`.`level` VALUES (3, 'FACILE');
